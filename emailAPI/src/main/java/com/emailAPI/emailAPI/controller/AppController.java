4677ccfbb5e746ce4dcbac73a114798a0f4e7b46
Administrator <Administrator@DESKTOP-QB4O65F.alphionsee.in>
2023-11-10 18:18:49 +0530

    this is my first commit for email API

---
+++ emailAPI/src/main/java/com/emailAPI/emailAPI/controller/AppController.java
@@ -0,0 +1,39 @@
+package com.emailAPI.emailAPI.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+
+	@PostMapping("/send_text_email")
+	
+	public String sendPlainTextEmail(@RequestParam("fromEmail") String fromEmail,
+			@RequestParam("toEmail") String toEmail) {
+
+		String from = fromEmail;
+		String to = toEmail;
+		System.out.println("from-email---" + from);
+		System.out.println("to--email-" + to);
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(from);
+		message.setTo(to);
+		message.setSubject("This is a plain text email");
+		message.setText("Hello guys! This is a plain text email.");
+
+		mailSender.send(message);
+
+		return "result";
+	}
+
+}
